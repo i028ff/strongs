1014f2cacf7d0cd115d05ccb11d1457832d4fa54
i028ff <kousuke111125@gmail.com>
2022-07-18 18:49:29 +0900

    getapi

---
+++ getapi.py
@@ -0,0 +1,42 @@
+from unittest import result
+from flask import Flask, render_template
+import googlemaps
+import pprint
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def getapi():
+    key = 'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM' # 上記で作成したAPIキーを入れる
+    client = googlemaps.Client(key) #インスタンス生成
+    loc = {'lat': 35.6288505, 'lng': 139.65863579999996} # 軽度・緯度を取り出す
+    place_results = client.places_nearby(location=loc, radius=1000, keyword='カフェ',language='ja') #半径1000m以内のカフェ情報を取得
+    #pprint.pprint(place_results)
+    results = []
+    photos = []
+    for place_result in place_results['results']:
+        results.append(place_result)
+        # 配列にphotosが存在しないとき、NO IMAGE画像を表示。
+        if not 'photos' in place_result.keys():
+            photo = 'https://hamaotoko.com/wp-content/uploads/2020/09/img_0961-scaled-e1599991033270.jpg'
+            photos.append(photo)
+        else:
+            #p_value = results['photos'][0]
+            #photos_photo_reference = p_value['photo_reference']
+            #print.pprint(photos_photo_reference)
+            #photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(photos_photo_reference,str(app.config['AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM']))
+            #配列に追加 
+            #photos.append(photo)
+            
+            p_value = place_result['photos'][0]['photo_reference']
+            photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM')
+            photos.append(photo)
+
+    #pprint.pprint(photos)
+    print(results)
+    return render_template('getapi.html',result=results,photos=photos)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
