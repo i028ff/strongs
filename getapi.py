b0d431c089e5ea0f57f3c329f7d17fd87b500755
i028ff <kousuke111125@gmail.com>
2022-07-20 01:21:41 +0900

    城間

--- getapi.py
+++ getapi.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from unittest import result
 from statistics import mean
 from flask import Flask, render_template
@@ -9,10 +10,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    key = 'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM' # 上記で作成したAPIキーを入れる
+
+    return render_template('getapi2.html')
+
+@app.route('/search_index', methods=['POST','GET'])
+def search_index(word):
+    word = request.form.get('word')
+    key = 'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM' #APIキー
+    geo_request_ur1 = 'https://get.geojs.io/v1/ip/geo.json'
+    data = requests.get(geo_request_ur1).json()
     client = googlemaps.Client(key) #インスタンス生成
-    loc = {'lat': 33.9563797, 'lng': 131.2725447} # 軽度・緯度を取り出す
-    place_results = client.places_nearby(location=loc, radius=10000, keyword='麺',language='ja') #半径1000m以内のカフェ情報を取得
+    loc = {'lat': data['latitude'], 'lng': data['longitude']} # 軽度・緯度を取り出す
+    place_results = client.places_nearby(location=loc, radius=100000, keyword='word',language='ja') #半径1000m以内のカフェ情報を取得
     #pprint.pprint(place_results)
     results = []
     photos = []
@@ -23,12 +32,6 @@ def index():
             photo = 'https://hamaotoko.com/wp-content/uploads/2020/09/img_0961-scaled-e1599991033270.jpg'
             photos.append(photo)
         else:
-            #p_value = results['photos'][0]
-            #photos_photo_reference = p_value['photo_reference']
-            #print.pprint(photos_photo_reference)
-            #photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(photos_photo_reference,str(app.config['AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM']))
-            #配列に追加 
-            #photos.append(photo)
             
             p_value = place_result['photos'][0]['photo_reference']
             photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM')
@@ -38,8 +41,10 @@ def index():
     #print(results)
     return render_template('getapi2.html',results=results,photos=photos)
 
+
 @app.route('/detail/<string:id>')
 def detail(id):
+
     key = 'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM' #APIキー
     url = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+id+"&fields=name%2Crating%2Cformatted_phone_number%2Cformatted_address&key="+key
     #url = "https://maps.googleapis.com/maps/api/place/detailes/xml?key="+key+"&"+id
@@ -49,5 +54,6 @@ def detail(id):
 
     print(response.text)
     return render_template('detail.html',detail=response)
+
 if __name__=='__main__':
     app.run(debug=True)
