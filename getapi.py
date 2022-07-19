26e26ddd7a4cb2a530d26a5f471c1acbed9ab7ed
i028ff <kousuke111125@gmail.com>
2022-07-19 21:39:52 +0900

    get

--- getapi.py
+++ getapi.py
@@ -11,17 +11,14 @@ def getapi():
     key = 'AIzaSyCqmC20D0M_x4rrJyAMgdvJaY7-4cXYNBM' # 上記で作成したAPIキーを入れる
     client = googlemaps.Client(key) #インスタンス生成
     loc = {'lat': 33.9563797, 'lng': 131.2725447} # 軽度・緯度を取り出す
-    place_results = client.places_nearby(location=loc, radius=1000, keyword='飲食店',language='ja') #半径1000m以内のカフェ情報を取得
+    place_results = client.places_nearby(location=loc, radius=100000, keyword='カフェ',language='ja') #半径10000m以内のカフェ情報を取得
     #pprint.pprint(place_results)
     results = []
     photos = []
     for place_result in place_results['results']:
-        results.append(place_result)
-        # 配列にphotosが存在しないとき、NO IMAGE画像を表示。
-        if not 'photos' in place_result.keys():
-            photo = 'https://hamaotoko.com/wp-content/uploads/2020/09/img_0961-scaled-e1599991033270.jpg'
-            photos.append(photo)
-        else:
+        if 'photos' in place_result.keys():
+            results.append(place_result)
+        
             #p_value = results['photos'][0]
             #photos_photo_reference = p_value['photo_reference']
             #print.pprint(photos_photo_reference)
